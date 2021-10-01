7380fc20c6428f0d17f044844c7444660fcdd531
Chathura <kymcwyapa@gmail.com>
2021-10-01 17:33:19 +0300

    Linting on boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { ReactElement } from "react";
 
 import logo from "./logo.svg";
 
 import "./App.css";
 
-function App() {
+function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
