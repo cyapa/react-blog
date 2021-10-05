2efe4dfad8a0d04f02301c5b161a481943ac3a7f
cyapa <kymcwyapa@gmail.com>
2021-10-05 19:45:10 +0300

    Schema updated

--- src/validators/blogSchema.ts
+++ src/validators/blogSchema.ts
@@ -5,12 +5,12 @@ const blogSchema = yup.object().shape({
     .string()
     .trim()
     .required("Blog title is required")
-    .max(50, "Only 50 characters allowed"),
+    .max(100, "Only 100 characters allowed"),
   content: yup
     .string()
     .trim()
     .required("Blog content is required")
-    .max(100, "Only 100 characters allowed"),
+    .max(1000, "Only 1000 characters allowed"),
 });
 
 export const INITIAL_VALUES = {
